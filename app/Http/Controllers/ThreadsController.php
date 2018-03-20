955a15efc0a165587e153352793cf21a1ee833e5
gretar <gretar95@gmail.com>
2018-03-20 08:57:59 +0000

    first commit

---
+++ app/Http/Controllers/ThreadsController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Channel;
+use App\Thread;
+use Illuminate\Http\Request;
+
+class ThreadsController extends Controller
+{
+	public function __construct()
+	{
+		$this->middleware("auth")->except(["index", "show"]);
+	}
+
+    public function index(Channel $channel)
+    {
+        if ($channel->exists) {
+            $threads = $channel->threads()->latest()->get();
+        } else {
+            $threads = Thread::latest()->get();
+        }
+
+    	return view("threads.index", compact("threads"));
+    }
+
+    public function show($channelId, Thread $thread)
+    {
+    	return view("threads.show", compact("thread"));
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            "title" => "required",
+            "body" => "required",
+            "channel_id" => "required|exists:channels,id"
+        ]);
+
+    	$thread = Thread::create([
+    		"user_id" => auth()->id(),
+            "channel_id" => request("channel_id"),
+    		"title" => request("title"),
+    		"body" => request("body")
+    	]);
+
+    	return redirect($thread->path());
+    }
+    public function create()
+    {
+        return view("threads.create");
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get("threads", "ThreadsController@index");
+Route::get("threads/create", "ThreadsController@create");
+Route::get("threads/{chennel}/{thread}", "ThreadsController@show");
+Route::post("threads", "ThreadsController@store");
+Route::get("threads/{channel}", "ThreadsController@index");
+Route::post("/threads/{channel}/{thread}/replies", "RepliesController@store");
+
+
+
+//kominn a þatt 12 min 1:04
+
+
+
